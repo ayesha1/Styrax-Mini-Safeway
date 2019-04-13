cc380ddd3390ad0686bb39a69ddc7cd0834cfdd5
Ryan <ryan.hopper-lowe@sjsu.edu>
2019-04-12 20:18:39 -0700

    Separated firebase-auth.js file and added firestore to project

--- Public/js/firebase-auth.js
+++ Public/js/firebase-auth.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
+  console.log(firebase.auth());
   // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
   // // The Firebase SDK is initialized and available here!
+  //
   firebase.auth().onAuthStateChanged(user => {
     if (user) {
       window.alert(user);
     } else {
       window.alert("not logged in");
     }
-  })
+    console.log(firebase.auth().currentUser)
+  });
   // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
   // firebase.messaging().requestPermission().then(() => { });
   // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
@@ -24,14 +27,35 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
+function handleLogin(){
+  let email = document.getElementById("login_email").value;
+  let password = document.getElementById("login_password").value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+    window.alert("Login success");
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert(errorCode, errorMessage);
+  });
+}
+
 function registerUser() {
   let email = document.getElementById("reg_email").value;
   let password = document.getElementById("reg_password").value;
 
   console.log(email, password);
-  firebase.auth().createUserWithEmailAndPassword(email, password).then(function () {
-    window.alert("success");
-  }).catch(function (error) {
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
     window.alert("Registration Unsuccessful");
+  });
+}
+
+function handleLogout() {
+
+  firebase.auth().signOut().then(function() {
+    window.alert("logout success");
+  }).catch(function(error) {
+    window.alert("logout Unsuccessful");
   });
 }
