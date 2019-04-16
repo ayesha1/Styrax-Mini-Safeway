bf7eac1344feda66cb749e4022430cc70e9a22d9
Ryan <ryan.hopper-lowe@sjsu.edu>
2019-04-15 17:50:31 -0700

    Connected user data to authenticated users

--- Public/js/firebase-auth.js
+++ Public/js/firebase-auth.js
@@ -1,23 +1,41 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   console.log(firebase.auth());
+  const auth = firebase.auth();
   const db = firebase.firestore();
   console.log(db);
 
   // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
   // // The Firebase SDK is initialized and available here!
   //
-  firebase.auth().onAuthStateChanged(user => {
+  auth.onAuthStateChanged(user => {
+    //if user is authenticated
     if (user) {
-      // window.alert(user);
-      document.getElementById("signIn").classList.add("hidden");
-      document.getElementById("signUp").classList.add("hidden");
-      document.getElementById("logOut").classList.remove("hidden");
-      if (window.location.href.includes("Home.html")) {
-        document.getElementById("userName").classList.remove("hidden");
-        document.getElementById("userName").innerHTML = "Hi!";
-        document.getElementById("guest_message").innerHTML = "You are Eligible for a 10% Discount on All Designated Items!";
-      }
+      //retrieve user's personal doc using their uid
+      var userRef = db.collection("users").doc(user.uid);
+      var userInfo;
+      //retrieve basic user info
+      userRef.get().then(function(doc) {
+        if (doc.exists) {
+          userInfo = doc.data().userInfo;
+          console.log(doc.data());
+          console.log(userInfo);
+        } else {
+          console.log("could not find user document");
+        }
+
+        document.getElementById("signIn").classList.add("hidden");
+        document.getElementById("signUp").classList.add("hidden");
+        document.getElementById("logOut").classList.remove("hidden");
+        if (window.location.href.includes("Home.html")) {
+          document.getElementById("userName").classList.remove("hidden");
+          document.getElementById("userName").innerHTML = "Hi " + userInfo.name + "!";
+          document.getElementById("guest_message").innerHTML = "You are Eligible for a 10% Discount on All Designated Items!";
+        }
+
+      }).catch(function(error) {
+        window.alert(error.code, error.message);
+      });
 
     } else {
       // window.alert("not logged in");
@@ -29,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById("guest_message").innerHTML = "Sign In and Receive Online Discounts!";
       }
     }
-    console.log(firebase.auth().currentUser)
+    console.log(auth.currentUser)
   });
   // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
   // firebase.messaging().requestPermission().then(() => { });
@@ -66,10 +84,30 @@ function handleLogin(){
 function registerUser() {
   let email = document.getElementById("reg_email").value;
   let password = document.getElementById("reg_password").value;
+  let newUser = {
+    name: document.getElementById("reg_name").value,
+    email: email,
+    address: {
+      street: document.getElementById("reg_address").value,
+      city: document.getElementById("reg_city").value,
+      state: document.getElementById("reg_state").value
+    }
+  };
 
   console.log(email, password);
   firebase.auth().createUserWithEmailAndPassword(email, password).then(function () {
-    window.location.href = "./Home.html";
+    let userId = firebase.auth().currentUser.uid;
+    console.log(userId);
+    firebase.firestore().collection("users").doc(userId).set({
+      "userInfo": newUser
+    }).then(function (docRef) {
+      window.alert("success");
+      console.log(docRef);
+    }).catch(function(error) {
+      window.alert(error.code, error.message);
+    })
+
+    // window.location.href = "./Home.html";
   })
   .catch(function (error) {
     window.alert("Registration Unsuccessful");
