af2e8764a11655e07522e2c36f7dbf0a22389843
Ryan <ryan.hopper-lowe@sjsu.edu>
2019-04-22 15:57:46 -0700

    implemented adding to cart and allows anonymous login

--- Public/js/firebase-auth.js
+++ Public/js/firebase-auth.js
@@ -17,40 +17,51 @@ document.addEventListener('DOMContentLoaded', function() {
       var userRef = db.collection("users").doc(user.uid);
       var userInfo;
       //retrieve basic user info
-      userRef.get().then(function(doc) {
-        if (doc.exists) {
-          userInfo = doc.data().userInfo;
-          console.log(doc.data());
-          console.log(userInfo);
-        } else {
-          console.log("could not find user document");
-        }
+      if (!user.isAnonymous) {
+        userRef.get().then(function(doc) {
+          if (doc.exists) {
+            userInfo = doc.data().userInfo;
+            console.log(doc.data());
+            console.log(userInfo);
+          } else {
+            console.log("could not find user document");
+          }
 
-        document.getElementById("signIn").classList.add("hidden");
-        document.getElementById("signUp").classList.add("hidden");
-        document.getElementById("logOut").classList.remove("hidden");
+          document.getElementById("signIn").classList.add("hidden");
+          document.getElementById("signUp").classList.add("hidden");
+          document.getElementById("logOut").classList.remove("hidden");
+          if (window.location.href.includes("Home.html")) {
+            document.getElementById("userName").classList.remove("hidden");
+            document.getElementById("userName").innerHTML = "Hi " + userInfo.name + "!";
+            document.getElementById("guest_message").innerHTML = "You are Eligible for a 10% Discount on All Designated Items!";
+            // loadHomeContent();
+          }
+
+
+        }).catch(function(error) {
+          window.alert(error.code, error.message);
+        });
+
+      } else {
+        console.log("signed in anonymously");
+        // window.alert("not logged in");
+        document.getElementById("logOut").classList.add("hidden");
+        document.getElementById("signIn").classList.remove("hidden");
+        document.getElementById("signUp").classList.remove("hidden");
         if (window.location.href.includes("Home.html")) {
-          document.getElementById("userName").classList.remove("hidden");
-          document.getElementById("userName").innerHTML = "Hi " + userInfo.name + "!";
-          document.getElementById("guest_message").innerHTML = "You are Eligible for a 10% Discount on All Designated Items!";
-          loadHomeContent();
+          document.getElementById("userName").classList.add("hidden");
+          document.getElementById("guest_message").innerHTML = "Sign In and Receive Online Discounts!";
+          // loadHomeContent();
         }
-
-
-      }).catch(function(error) {
-        window.alert(error.code, error.message);
-      });
+      }
+      loadHomeContent();
 
     } else {
-      // window.alert("not logged in");
-      document.getElementById("logOut").classList.add("hidden");
-      document.getElementById("signIn").classList.remove("hidden");
-      document.getElementById("signUp").classList.remove("hidden");
-      if (window.location.href.includes("Home.html")) {
-        document.getElementById("userName").classList.add("hidden");
-        document.getElementById("guest_message").innerHTML = "Sign In and Receive Online Discounts!";
-        loadHomeContent();
-      }
+
+      auth.signInAnonymously().catch(error => {
+        window.alert(error.code + ": " + error.message);
+      });
+
     }
 
 
@@ -59,44 +70,49 @@ document.addEventListener('DOMContentLoaded', function() {
 
     console.log(auth.currentUser);
 
-    var populateListFields = function (items) {
-      setTimeout(function (){
-        console.log(items);
-        for (let i in items) {
-          $('#PopularAisle').append($('<div class="itemCard">').attr("id", ("item" + i)));
-          $(("#" + ("item" + i))).append(
-            $('<a class="itemLink">').attr({"href": ("link" + i), "id": ("link" + i)}),         //link to Item i?
-            $('<h5 class="itemPrice">').text("$" + items[i].originalPrice.toFixed(2)),                                         //retrieve Item Price at "$1.00"
-            $('<button class="addbtn" onlick="">').text("Add")
-          );                                                                                    //add item onclick="addfunction"
-          $(("#" + ("link" + i))).append(
-            $('<img class="itemImg img-responsive">').attr(
-              {"src": items[i].imageUrl, "alt" : ("item"+i)}),        //retrieve item image here
-              $('<h4 class="itemName">').text(items[i].name)
-          );
-        }
-      }, 1000);
-    }
+    addItemToCart = function(item) {
+      let cartsRef = db.collection('carts');
+      let productsRef = db.collection('products');
+      let itemRef = productsRef.doc(item.name);
+      console.log(item);
 
-    var loadHomeContent = function getAllProducts() {
-      loaded_items = [];
-      let i = 0;
-      var productsRef = db.collection("products").limit(20)
-      .get().then(querySnapshot => {
-        querySnapshot.forEach(doc => {
-          let item = doc.data();
-          // console.log(doc.id + ": ", doc.data());
-          loaded_items[i] = item;
-          i++;
-        })
-      }).catch(error => {
+      var userCart;
+      cartsRef.doc(auth.currentUser.uid)
+      .get()
+      .then(doc => {
+
+        console.log(doc);
+        if (doc.exists) {
+          userCart = doc.data();
+          if (userCart.items[item.name]) {
+            userCart.items[item.name].count++;
+          } else {
+            userCart.items[item.name] = {
+              ref: itemRef,
+              count: 1
+            }
+          }
+          console.log(userCart);
+          cartsRef.doc(auth.currentUser.uid).update(userCart);
+
+        } else {
+
+          let newCart = {
+            items: {}
+          };
+
+          newCart.items[item.name] = {
+            ref: itemRef,
+            count: 1
+          };
+          console.log(newCart);
+          cartsRef.doc(auth.currentUser.uid).set(newCart);
+
+        }
+      })
+      .catch(error => {
         window.alert(error.code + ": " + error.message);
       });
-      console.log(loaded_items);
-      // pageSetup(loaded_items);
-      listItems = loaded_items;
-      console.log(loaded_items.length);
-      populateListFields(loaded_items);
     }
 
 
@@ -117,6 +133,52 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
 });
+
+var addItemToCart;
+
+var populateListFields = function (items) {
+  setTimeout(function (){
+    console.log(items);
+    for (let i in items) {
+      $('#PopularAisle').append($('<div class="itemCard">').attr("id", ("item" + i)));
+      $(("#" + ("item" + i))).append(
+        $('<a class="itemLink">').attr({"href": ("link" + i), "id": ("link" + i)}),         //link to Item i?
+        $('<h5 class="itemPrice">').text("$" + items[i].originalPrice.toFixed(2)),                                         //retrieve Item Price at "$1.00"
+        $('<button class="addbtn" id="addItem' + i + '" onlick="addItemToCart(' + i + ')">').text("Add")
+      );                                                                                    //add item onclick="addfunction"
+      $(("#" + ("link" + i))).append(
+        $('<img class="itemImg img-responsive">').attr(
+          {"src": items[i].imageUrl, "alt" : ("item"+i)}),        //retrieve item image here
+          $('<h4 class="itemName">').text(items[i].name)
+      );
+
+      $('#' + ('addItem' + i)).on('click', function () {
+        addItemToCart(items[i]);
+      })
+    }
+  }, 1000);
+}
+
+var loadHomeContent = function loadHomeContent() {
+  loaded_items = [];
+  let i = 0;
+  let productsRef = firebase.firestore().collection("products").limit(20)
+  .get().then(querySnapshot => {
+    querySnapshot.forEach(doc => {
+      let item = doc.data();
+      // console.log(doc.id + ": ", doc.data());
+      loaded_items[i] = item;
+      i++;
+    })
+  }).catch(error => {
+    window.alert(error.code + ": " + error.message);
+  });
+  console.log(loaded_items);
+  // pageSetup(loaded_items);
+  listItems = loaded_items;
+  console.log(loaded_items.length);
+  populateListFields(loaded_items);
+}
 
 function handleLogin(){
   let email = document.getElementById("login_email").value;
