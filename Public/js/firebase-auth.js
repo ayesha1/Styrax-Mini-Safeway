198509ec9e286d6f4999acda490c14a69d96917a
Ryan <ryan.hopper-lowe@sjsu.edu>
2019-04-26 18:37:16 -0700

    removed outdated api key

--- Public/js/firebase-auth.js
+++ Public/js/firebase-auth.js
@@ -1,13 +1,3 @@
-
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-  apiKey: '### FIREBASE API KEY ###',
-  authDomain: '### FIREBASE AUTH DOMAIN ###',
-  projectId: '### CLOUD FIRESTORE PROJECT ID ###'
-});
-
-var db = firebase.firestore();
-
 document.addEventListener('DOMContentLoaded', function() {
   console.log(firebase.auth());
   const auth = firebase.auth();
