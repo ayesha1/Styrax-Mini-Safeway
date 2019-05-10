99de0ee1556e45e29e5300d13f036fb948e48e82
Ryan <ryan.hopper-lowe@sjsu.edu>
2019-05-09 17:44:47 -0700

    added logic to highlight active nav elements

--- Public/components/components.js
+++ Public/components/components.js
@@ -1,8 +1,16 @@
 
-// $(document).ready(function () {
-  $('#styrax-nav').load('/components/templates/navbar.html #styraxNav');
+$('#styrax-nav').load('/components/templates/navbar.html #styraxNav');
 
-  $('#styrax-footer').load('/components/templates/footer.html #styraxFooter');
+$('#styrax-footer').load('/components/templates/footer.html #styraxFooter');
 
-  $('#styrax-searchbar').load('/components/templates/searchbar.html #nameSearch');
-// });
+$('#styrax-searchbar').load('/components/templates/searchbar.html #nameSearch');
+
+$(document).ready(function() {
+  if (window.location.href.includes("Cart.html")) {
+    $('#navCart').addClass('activeNav');
+  } else if (window.location.href.includes("SignIn.html")) {
+    $('#navLogin').addClass('activeNav');
+  } else if (window.location.href.includes("SignUp.html")) {
+    $('#navSignUp').addClass('activeNav');
+  }
+})
